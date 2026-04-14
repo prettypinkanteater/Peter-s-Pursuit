c69c301c206644356a5786dec624d3f2131152e0
prettypinkanteater <audrh12@uci.edu>
2026-04-14 16:55:03 -0700

    Real GC andLocator Setup

---
+++ petey/Assets/Scripts/Locator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locator : MonoBehaviour
+{
+    public static Locator Instance { get; private set; }
+
+    public GameController controller { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
